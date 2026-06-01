e00e80552bee64bb196aea49ff7fd559e915986c
Sam <117794654+Sampriv@users.noreply.github.com>
2026-06-01 12:42:37 -0400

    logic for play

--- main.js
+++ main.js
@@ -14,3 +14,38 @@ console.log(getHumanChoice())
 
 const humanScore = 0;
 const computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.lower();
+    computerChoice = computerChoice.lower();
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        return "You lose! Paper beats rock";
+        computerScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        return "You win! Paper beats rock";
+        humanScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        return "You win! Rock beats scissors";
+        humanScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        return "You lose! Scissors beats paper";
+        computerScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        return "You Win! Scissors beats paper";
+        humanScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        return "You win! Rock beats scissors";
+        computerScore++;
+    }
+    else {
+        return "Same move, play again!"
+    }
+}
