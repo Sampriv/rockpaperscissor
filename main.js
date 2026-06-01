89edc4de5134faf1b075bf246d3621fd6461d5c6
Sam <117794654+Sampriv@users.noreply.github.com>
2026-06-01 12:30:55 -0400

    get humanchoice

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 console.log("Hello World");
 
-function getComputerChoice {
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]
-    const index = Math.random()*3
+    const index = (Math.random()*3)
     return choices[Math.floor(index)]
 }
 
+function getHumanChoice() {
+    return(prompt("Please choose rock, paper or scissors."))
+}
+
+console.log(getHumanChoice())
+
+const humanScore = 0;
+const computerScore = 0;
