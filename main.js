fdc4c3d66506a67bb9934dd90254f1adb30e7140
Sam <117794654+Sampriv@users.noreply.github.com>
2026-06-01 12:26:13 -0400

    getcompchoice

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+function getComputerChoice {
+    const choices = ["rock", "paper", "scissors"]
+    const index = Math.random()*3
+    return choices[Math.floor(index)]
+}
+
